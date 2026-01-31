6ad6d242abd9e9f128c85db3c9be4294d9b6d01a
blackSwan566 <57305281+blackSwan566@users.noreply.github.com>
2026-01-31 13:28:22 +0100

    last commit here

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -6,6 +6,7 @@ public class GameManager : MonoBehaviour
     // Singleton: Erlaubt einfachem Zugriff von überall
     public static GameManager Instance { get; private set; }
 
+    public int life = 5;
     public int score = 0;
     public int counter = 0;
     public TextMeshProUGUI scoreText;
